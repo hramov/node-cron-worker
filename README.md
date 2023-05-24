4818f25dfc8c759d7b98207ffa376a567c4c8230
hramov <trykhramov@gmail.com>
2023-05-24 13:37:04 +0700

    updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node Cron Worker Lib 
+- Write cron jobs with typescript
+- Run jobs in parallel with worker_threads in two modes
--- lib/interface.ts
+++ lib/interface.ts
@@ -1,5 +1,8 @@
+import {INodeCronWorkerScheduleOptions, INodeCronWorkerTask} from "./node-cron-worker";
+
 export interface INodeCronWorker {
     setMode(mode: Modes): void
+    schedule(cron: string, func: Function, funcParams: any, options: INodeCronWorkerScheduleOptions): Promise<INodeCronWorkerTask>
 }
 
 export const enum Modes {
